0a25377107879ac025e0cbe1460bba92087f50cb
Дарья Петрова <petrova.dv@phystech.edu>
2021-03-12 18:36:12 +0300

    Add Home page and Sign in

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,28 @@
+from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Daria'}
+    expenses = [ 
+        {
+            'category': 'products',
+            'body': '23$'
+        },
+        {
+            'category' : 'clothes',
+            'body': '100$'
+        },
+        {
+            'category' : 'beauty products',
+            'body': '58$'
+        }
+    ]
+    return render_template('index.html', title = 'Home',  user = user, expenses = expenses)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Sign in', form=form)
