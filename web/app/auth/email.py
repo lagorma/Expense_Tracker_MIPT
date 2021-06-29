d3c9bcf851b0b22cf87f82e76ff3c1251c1612ef
Дарья Петрова <petrova.dv@phystech.edu>
2021-06-29 10:08:16 +0300

    Fix reset password

--- web/app/auth/email.py
+++ web/app/auth/email.py
@@ -4,16 +4,7 @@ from app.email import send_mail
 
 def send_password_reset_email(user):
     token = user.get_reset_password_token()
-    print(token)
     send_mail('[Microblog] Reset Your Password',
                sender=current_app.config['ADMINS'][0],
                recipients=[user.email],
                text_body=render_template('email/reset_password.txt', user=user, token=token))
-              # html_body=render_template('email/reset_password.html', 
-                                         #user=user, token=token))
-    #BCC = 'receiver2@test.net'
-    #SUBJECT = 'GmailのSMTPサーバ経由'
-    #BODY = 'pythonでメール送信'
-    #msg = create_message(current_app.config['ADMINS'][0],[user.email], BCC, SUBJECT, BODY)
-    #send_mail(current_app.config['ADMINS'][0], [user.email], msg)
-    #send_mail()
